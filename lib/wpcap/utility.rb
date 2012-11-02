906a2578d608ad4be20eaf5531fe48896fd3a758
Russell Osborne <russell@burningpony.com>
2012-11-02 14:47:40 -0400

    Adding pretty colors.

--- lib/wpcap/utility.rb
+++ lib/wpcap/utility.rb
@@ -4,11 +4,16 @@ class Wpcap::Utility
     puts red("****#{text}****")
   end
   
+  def self.question(text)
+    puts blue("****#{text}****")
+  end
+  
   def self.colorize(text, color_code)
     "\e[#{color_code}m#{text}\e[0m"
   end
 
   def self.red(text); self.colorize(text, 31); end
   def self.green(text); self.colorize(text, 32); end
-
+  def self.blue(text); self.colorize(text, 34); end
+  
 end
