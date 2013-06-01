2d823941def26ea9edc7d2f2c670e79ef7f113f5
Russell Osborne <russell@burningpony.com>
2013-06-01 14:42:43 -0400

    Updating New Relic call for nginx template

--- lib/wpcap/recipes/wordpress.rb
+++ lib/wpcap/recipes/wordpress.rb
@@ -7,18 +7,18 @@ Capistrano.configuration(:must_exist)
 
 configuration.load do
   extraction_terms = [ {"DB_CHARSET" => "encoding"}, {"DB_NAME" => "database"} , {"DB_USER" => "username"} ,  {"DB_PASSWORD" => "password" } , {"DB_HOST" => "host"}]
-  
+
   set_default :uploads_path, "wp-content/uploads"
 
 
 
 
-  namespace :db do 
+  namespace :db do
     desc "Alias for db:mysql:pull"
     task :pull , :roles => :db do
       db.mysql.pull
     end
-  
+
     desc "Alias for db:mysql:push"
     task :push , :roles => :db do
       db.mysql.push
@@ -32,24 +32,24 @@ configuration.load do
 
       run "rm -Rf #{shared_path}/pids"
       run "rm -Rf #{shared_path}/system"
-      
+
       # create shared directories
       run "mkdir -p #{shared_path}/uploads"
       run "mkdir -p #{shared_path}/cache"
       run "mkdir -p #{shared_path}/logs"
       run "mkdir -p #{shared_path}/config"
       run "mkdir -p #{backups_path}"
-      
+
       # setup for wp total cache
       run "touch #{shared_path}/config/nginx.conf"  if  custom_nginx
-      
+
       # set correct permissions
       run "chmod -R 755 #{latest_release}/app/wp-content/plugins"
       run "chmod -R 775 #{shared_path}/uploads"
       run "chmod -R 775 #{shared_path}/cache"
       run "chmod -R 755 #{latest_release}"
     end
-  
+
     desc "[internal] Touches up the released code. This is called by update_code after the basic deploy finishes."
     task :finalize_update, :roles => :web, :except => { :no_release => true } do
       # remove shared directories
@@ -59,14 +59,14 @@ configuration.load do
       # Removing cruft files.
       run "rm -Rf #{latest_release}/license.txt"
       run "rm -Rf #{latest_release}/readme.html"
-    
+
     end
     desc "[internal] Verify Code and repo is ready to be released"
     task :check_code, :except => { :no_release => true } do
       #make sure the user has actually set up the wordpress install
       unless  File.exists?("app/wp-config.php")
         Wpcap::Utility.error("This Wordpress Installation does not appear to have a wp-config.php file (Please create one at app/wp-config.php )")
-        abort 
+        abort
       end
     end
     before "deploy", "deploy:check_code"
@@ -74,7 +74,7 @@ configuration.load do
   end
 
   namespace :wordpress do
-  
+
     desc "Links the correct settings file"
     task :symlink, :roles => :web, :except => { :no_release => true } do
       run "ln -nfs #{shared_path}/uploads #{latest_release}/app/#{uploads_path}"
@@ -82,8 +82,8 @@ configuration.load do
 
       if  custom_nginx
         run "rm -f #{shared_path}/config/nginx.conf"
-      run "ln -nfs #{shared_path}/config/nginx.conf #{latest_release}/app/nginx.conf"
-    end
+        run "ln -nfs #{shared_path}/config/nginx.conf #{latest_release}/app/nginx.conf"
+      end
 
     end
 
@@ -93,7 +93,7 @@ configuration.load do
       wpconfig = run_locally "cat app/wp-config.php"
       stage_config = "/tmp/wp-config.#{stage}"
       db.mysql.prepare_env
-      
+
       extraction_terms.each do |env|
         term =  env.keys.first
         sym = env.values.first
@@ -105,23 +105,23 @@ configuration.load do
       upload stage_config, "#{latest_release}/app/wp-config.php"
       run_locally "rm #{stage_config}"
     end
-    
+
     desc "Sync Local Assets to Remote"
-    task "assets_push", :roles => :web do 
+    task "assets_push", :roles => :web do
       servers = find_servers_for_task(current_task)
       servers.each do |server|
         run_locally "if [ -d app/#{uploads_path} ]; then rsync -avhru app/#{uploads_path}/* -delete -e 'ssh -p #{port}' #{user}@#{server}:#{shared_path}/uploads; fi"
       end
     end
-  
+
     desc "Sync Remote Assets to Local"
-    task "assets_pull", :roles => :web do 
+    task "assets_pull", :roles => :web do
       servers = find_servers_for_task(current_task)
       servers.each do |server|
         run_locally "if [ -d app/#{uploads_path} ]; then rsync -avhru -delete -e 'ssh -p #{port}' #{user}@#{server}:#{shared_path}/uploads/*    app/#{uploads_path}; fi"
       end
     end
-  
+
     desc "Extract PHP DB Env Vars"
     task :extract_local_db,  :except => { :no_release => true } do
       wpconfig = run_locally "cat app/wp-config.php"
@@ -131,13 +131,13 @@ configuration.load do
         sym = env.values.first
         dev_env["development"][sym] = wpconfig.match(/define\('#{term}', '(.{1,16})'\);/)[1]
       end
-     
+
       dev_env["development"]["prefix"] = wpconfig.match(/\$table_prefix  = '(.{1,16})';/)[1]
       db.mysql.update_db_config(dev_env)
       print dev_env
     end
     before "deploy:setup", "wordpress:extract_local_db"
-  
+
     desc "Set URL in database"
     task :updatedb, :roles => :db, :except => { :no_release => true } do
       db.mysql.prepare_env
@@ -145,7 +145,7 @@ configuration.load do
       run "mysql -u #{db_username} -p #{db_database} -e 'UPDATE #{db_prefix}options SET option_value = \"#{application_url}\" WHERE option_name = \"siteurl\" OR option_name = \"home\"'" do |ch, stream, out|
         ch.send_data "#{db_password}\n" if out =~ /^Enter password:/
         puts out
-      end 
+      end
 
       run "mysql -u #{db_username} -p #{db_database} -e 'UPDATE #{db_prefix}posts SET post_content = REPLACE(post_content, \"#{local_uri}\", \"#{application_url}\")'" do |ch, stream, out|
         ch.send_data "#{db_password}\n" if out =~ /^Enter password:/
@@ -154,7 +154,7 @@ configuration.load do
     end
     after "db:mysql:push", "wordpress:updatedb"
     after "db:mysql:push", "wordpress:assets_push"
-  
+
     desc "Set URL in local database"
     task :update_local_db, :except => { :no_release => true } do
       db.mysql.prepare_env(:development)
@@ -166,14 +166,14 @@ configuration.load do
 
     after "db:mysql:pull", "wordpress:update_local_db"
     after "db:mysql:pull", "wordpress:assets_pull"
-  
+
     desc "[internal] Protect system files"
     task :protect, :except => { :no_release => true } do
       run "chmod 444 #{latest_release}/app/wp-config.php*"
       run "cd #{current_path} && chown -R #{user} . && find . -type d -print0 | xargs -0 chmod 755"
       run "cd #{shared_path}/uploads && chown -R #{user} . && chmod 755 -R ."
     end
-    
+
     desc "[internal] get current wp template from local db"
     task :current_wp_template, :except => { :no_release => true } do
       db.mysql.prepare_env(:development)
@@ -185,7 +185,7 @@ configuration.load do
       end
     end
     before "nginx:setup", "wordpress:current_wp_template"
-    
+
     after "wordpress:symlink", "wordpress:create_config"
     after "wordpress:create_config", "wordpress:protect"
     after "deploy", "wordpress:symlink"
